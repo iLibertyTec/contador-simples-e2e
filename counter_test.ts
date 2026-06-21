563aaaf8816130f43ef341b121193108454b453d
willantunes <antunes406@gmail.com>
2026-06-21 19:58:39 +0200

    consolida contador em memória com incremento explícito
    iFactory plan 6eb64691-bfdc-4612-9e21-05a6b69b3027

--- counter_test.ts
+++ counter_test.ts
@@ -1,12 +1,31 @@
 import { assertEquals } from "@std/assert";
 import { formatCounterMessage, VisitCounter } from "./counter.ts";
 
-Deno.test("VisitCounter increments", () => {
-  const c = new VisitCounter();
-  assertEquals(c.state.visits, 0);
-  c.recordVisit("a");
-  assertEquals(c.state.visits, 1);
-  assertEquals(c.state.lastVisitor, "a");
+Deno.test("VisitCounter inicia com visits igual a 0", () => {
+  const counter: VisitCounter = new VisitCounter();
+
+  assertEquals(counter.state.visits, 0);
+});
+
+Deno.test("VisitCounter incrementVisit incrementa uma visita por chamada", () => {
+  const counter: VisitCounter = new VisitCounter();
+
+  const firstState = counter.incrementVisit();
+  const secondState = counter.incrementVisit();
+
+  assertEquals(firstState.visits, 1);
+  assertEquals(secondState.visits, 2);
+  assertEquals(counter.state.visits, 2);
+});
+
+Deno.test("VisitCounter recordVisit mantém compatibilidade e registra último visitante", () => {
+  const counter: VisitCounter = new VisitCounter();
+
+  const state = counter.recordVisit("a");
+
+  assertEquals(state.visits, 1);
+  assertEquals(state.lastVisitor, "a");
+  assertEquals(counter.state.lastVisitor, "a");
 });
 
 Deno.test("formatCounterMessage pt-BR", () => {
